1e2e3f1e311e978a035971e1c298667444df8fbe
wuchengqian <chengqian.wu@smartmetering.com.cn>
2019-03-06 11:27:50 +0800

    add files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int result = Catch::Session().run(argc, argv);
+	
+	getchar();
+
+	return (result < 0xff ? result : 0xff);
+}
