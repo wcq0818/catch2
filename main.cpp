354cf845b045825cde5e600d90831c61842b189d
wuchengqian <chengqian.wu@wasion.com>
2019-04-23 14:38:10 +0800

    add catch2-test.props and posix_time.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
-#include <stdio.h>
+#include "catch.hpp"
 
 int main(int argc, char* argv[])
 {
 	int result = Catch::Session().run(argc, argv);
-	
-	getchar();
 
 	return (result < 0xff ? result : 0xff);
 }
